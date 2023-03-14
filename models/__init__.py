211e6c20a349a91e29247d1b04076f9ffb396413
nanasei0548 <nanasei0548@gmail.com>
2023-03-14 20:21:14 +0000

    images

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 27 13:56:43 2020
+@authors: Robinson Montes
+          Carlos Murcia
+"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
